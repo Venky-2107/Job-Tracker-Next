ab407e93e968d65509525684303cae29627d2af7
vcherukuri-evoke <vcherukuri@loop.tv>
2026-07-28 17:36:08 +0530

    websocket

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,42 @@
 "use client";
 
+import { useEffect } from "react";
 import { useFetchApplicationsQuery } from "@/store/applicationApi";
 import { ApplicationRequest } from "@/types";
 import ApplicationCard from "@/components/ApplicationCard";
 import { useRouter } from "next/navigation";
 import { useAppDispatch } from "@/hooks/redux";
 import { logout } from "@/store/authSlice";
+import { api } from "@/store/api";
 
 export default function Dashboard() {
   const { data, isLoading, isError } = useFetchApplicationsQuery(undefined);
   const router = useRouter();
   const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const match = document.cookie.match(/(?:^|;\s*)token=([^;]*)/);
+    const token = match ? match[1] : null;
+
+    const ws = new WebSocket(
+      `wss://job-tracker-production-e526.up.railway.app/application/ws?token=${token}`,
+    );
+
+    ws.onmessage = (event) => {
+      const { event: type } = JSON.parse(event.data);
+      const WS_EVENTS = [
+        "application_created",
+        "application_updated",
+        "application_deleted",
+      ];
+      if (WS_EVENTS.includes(type)) {
+        // invalidate RTK Query cache to trigger refetch
+        dispatch(api.util.invalidateTags(["Applications"]));
+      }
+    };
+
+    return () => ws.close();
+  }, []);
 
   console.log(data);
   if (isLoading) return <p>Loading...</p>;
